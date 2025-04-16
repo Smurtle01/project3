c357f90abd2c82d7a00c828e338add3b97561370
Smurtle01 <smurtle01@gmail.com>
2025-04-15 19:26:35 -0500

    updated script.js
    added the google maps scripts to the .js file.

---
+++ js/script.js
@@ -0,0 +1,37 @@
+ let map;
+ 
+ function initMap() {
+  const location = { lat: 37.7749, lng: -122.4194 };
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: location,
+  });
+
+  // Feature 1: Custom Marker
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    title: "We are here!",
+  });
+
+  // Feature 2: Info Window
+  const infoWindow = new google.maps.InfoWindow({
+    content: "<p>Welcome to our location!</p>",
+  });
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+
+  // Feature 3: Circle around location
+  new google.maps.Circle({
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.2,
+    map,
+    center: location,
+    radius: 2000,
+  });
+}
